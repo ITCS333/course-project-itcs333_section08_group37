c74c6c85b8762ab1014a5f621c8fde9a6b357c02
Zahraa028 <202005540@stu.uob.edu.bh>
2025-12-03 15:24:42 +0300

    Update db.php

--- src/common/db.php
+++ src/common/db.php
@@ -1,1 +1,19 @@
+<?php
+function getDBConnection() {
+    $host = "localhost";
+    $db   = " ";
+    $user = "root";
+    $pass = "";
+    $charset = "utf8mb4";
+
+    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
+    $pdo = new PDO($dsn, $user, $pass, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]);
+
+    return $pdo;
+}
+?>
 
