354fdcb615332096bc0b3bc17ed198e59909faaf
Zahraa028 <202005540@stu.uob.edu.bh>
2025-12-13 11:32:26 +0300

    Update list.js

--- src/resources/list.js
+++ src/resources/list.js
@@ -1,25 +1,38 @@
+
+function createResourceArticle(resource) {
+    const li = document.createElement('li');
+    li.innerHTML = `
+        <a href="Details.html?id=${resource.id}">${resource.title}</a>
+        <p>${resource.description}</p>
+    `;
+    return li;
+}
+
+
+window.createResourceArticle = createResourceArticle;
+
 const apiUrl = 'index.php';
 
 function loadResources() {
+    const list = document.getElementById('resourcesList');
+    list.innerHTML = '';
+
     fetch(apiUrl)
         .then(res => res.json())
         .then(data => {
-            const list = document.getElementById('resourcesList');
-            list.innerHTML = '';
-            if(data.success) {
+            if (data.success && data.data.length > 0) {
                 data.data.forEach(res => {
-                    const li = document.createElement('li');
-                    li.innerHTML = `
-                        <a href="Details.html?id=${res.id}">${res.title}</a>
-                        <p>${res.description}</p>
-                    `;
+                    const li = createResourceArticle(res);
                     list.appendChild(li);
                 });
             } else {
                 list.innerHTML = '<li>No resources available</li>';
             }
+        })
+        .catch(err => {
+            console.error('Error loading resources:', err);
+            list.innerHTML = '<li>Error loading resources</li>';
         });
 }
-
 
 loadResources();
