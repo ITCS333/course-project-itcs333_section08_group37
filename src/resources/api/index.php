826e70a044b689677d39ce003eeda48064d753db
Zahraa028 <202005540@stu.uob.edu.bh>
2025-12-18 20:33:44 +0300

    Update index.php

--- src/resources/api/index.php
+++ src/resources/api/index.php
@@ -1,8 +1,20 @@
 <?php
+session_start(); 
+
+
+if(!isset($_SESSION['user'])) {
+    $_SESSION['user'] = 'guest';
+}
+
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+
+
+
+
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
@@ -25,7 +37,7 @@ function sendResponse($data, $statusCode = 200) {
 }
 
 function validateUrl($url) {
-    // يقبل روابط كاملة أو تبدأ بـ www
+    
     return filter_var($url, FILTER_VALIDATE_URL) || preg_match('/^(www\.)[a-z0-9\-]+\.[a-z]{2,}/i', $url);
 }
 
